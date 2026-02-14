d1ad580470ca0dd4bb6e48dc94a3c361d3629fce
pnovillof <pnovillof@gmail.com>
2026-02-14 16:08:02 -0500

    v3 - Codigo Creado

--- Código/Codigo.py
+++ Código/Codigo.py
@@ -1,7 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Editor de Spyder
+import random #Libreria para selección computadora de (1) Piedra, (2) Papel, (3) Tijera
 
-Este es un archivo temporal.
-"""
+print("PIEDRA, PAPEL O TIJERA") #Mostramos mensaje para que usuario sepa que hacer
+print("Seleccione una opción:")
+print("1 = Piedra")
+print("2 = Papel")
+print("3 = Tijera")
 
+A = int(input("Ingrese su opción (1-3): ")) #Solicito ingresar la selección de usuario
+
+B = random.randint(1, 3) #Proceso interno de computador de selección de opción
+print("\nComputadora eligió:", B)
+
+#Creamos la estructura condicional, junto con una opción si alguien mete mal el dedo
+if A == B:
+    print("Resultado: EMPATE")
+
+elif A == 1 and B == 2:
+    print("Resultado: GANA COMPUTADORA")
+
+elif A == 2 and B == 3:
+    print("Resultado: GANA COMPUTADORA")
+
+elif A == 3 and B == 1:
+    print("Resultado: GANA COMPUTADORA")
+
+elif A == 1 and B == 3:
+    print("Resultado: GANA USUARIO")
+
+elif A == 2 and B == 1:
+    print("Resultado: GANA USUARIO")
+
+elif A == 3 and B == 2:
+    print("Resultado: GANA USUARIO")
+
+else:
+    print("Opción inválida")
