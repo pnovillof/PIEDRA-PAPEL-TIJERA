ef26afd9869bf333555b2dc4c50398f21050b269
pnovillof <pnovillof@gmail.com>
2026-03-01 09:36:12 -0500

    v8 - Organización de código con estructura de funciones

--- Código/Codigo.py
+++ Código/Codigo.py
@@ -1,6 +1,33 @@
 import random #Libreria para selección computadora de (1) Piedra, (2) Papel, (3) Tijera
 
-def resultado(A,B):
+def main():
+
+
+    print("PIEDRA, PAPEL O TIJERA") #Mostramos mensaje para que usuario sepa que hacer
+    print("Seleccione una opción:")
+    print("1 = Piedra")
+    print("2 = Papel")
+    print("3 = Tijera")
+
+
+    while True:
+        try:
+            A = int(input("Ingrese su opción (1-3): ")) #Solicito ingresar la selección de usuario
+            if A in [1, 2, 3]: #Uso de lista
+                break # selección correcta
+            else:
+                print ("Opción inválida. Intente nuevamente.")
+        except ValueError:
+                print("Debe ingresar un número válido.")
+        
+    B = random.randint(1, 3) #Proceso interno de computador de selección de opción
+    
+    print("\nComputadora eligió:", B)
+    
+    resultado(A, B)
+    
+
+def resultado(A,B): # creamos la subrutina que determina el resultado
     #Creamos la estructura condicional, junto con una opción si alguien mete mal el dedo
     if A == B:
         print("Resultado: EMPATE")
@@ -25,27 +52,6 @@ def resultado(A,B):
 
     else:
         print("Opción inválida")
-
-
-print("PIEDRA, PAPEL O TIJERA") #Mostramos mensaje para que usuario sepa que hacer
-print("Seleccione una opción:")
-print("1 = Piedra")
-print("2 = Papel")
-print("3 = Tijera")
-
-
-while True:
-    try:
-        A = int(input("Ingrese su opción (1-3): ")) #Solicito ingresar la selección de usuario
-        if A in [1, 2, 3]: #Uso de lista
-            break # selección correcta
-        else:
-            print ("Opción inválida. Intente nuevamente.")
-    except ValueError:
-            print("Debe ingresar un número válido.")
-    
-B = random.randint(1, 3) #Proceso interno de computador de selección de opción
-
-print("\nComputadora eligió:", B)
-
-resultado(A, B)
+        
+if __name__ == "__main__": # cuando el archivo se ejecuta directamente. Si se importa desde otro archivo, no ejecuta main().
+    main()
